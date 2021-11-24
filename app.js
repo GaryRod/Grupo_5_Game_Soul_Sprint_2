0bb5cacb6088869baf7446b8e2203a520abc7876
Julianarieltorres998 <julianarieltorres@gmail.com>
2021-11-24 18:31:46 -0300

    Añadido Express

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.static('views'));
+
+app.listen(process.env.PORT || 3000, () => console.log("Servidor funcionando en el puerto 3000 o el puerto definido en Heroku"));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/home.html');
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html');
+})
+
+app.get('/productcart', (req, res) => {
+    res.sendFile(__dirname + '/views/productCart.html');
+})
+
+app.get('/productdetail', (req, res) => {
+    res.sendFile(__dirname + '/views/productDetail.html');
+})
